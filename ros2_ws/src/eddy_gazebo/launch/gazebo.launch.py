72ba9744cf2acc966961f0017812fcca05926513
Michael Jenkin <jenkin@yorku.ca>
2024-03-28 14:10:26 -0400

    Robot she is up and running (in simulation)

--- ros2_ws/src/eddy_gazebo/launch/gazebo.launch.py
+++ ros2_ws/src/eddy_gazebo/launch/gazebo.launch.py
@@ -8,14 +8,24 @@ from launch_ros.actions import Node
 import xacro
 
 def generate_launch_description():
-    urdf = os.path.join(get_package_share_directory('eddy_gazebo'), 'eddy.gazebo.xacro')
+
+    # get the robot description and customize it
+    urdf = os.path.join(get_package_share_directory('eddy_description'), 'eddy.urdf.xacro')
     print(urdf)
-    robot_desc = xacro.process_file(urdf , mappings={
-        'robot_name' : 'eddy2a',
+    robot_description = xacro.process_file(urdf , mappings={
+        'robot_name' : 'Eddy2a',
         'gps_latitude_origin' : '43.77067122214868',
         'gps_longitude_origin' : '-79.50699748853243'
         }).toxml()
-    print(robot_desc)
+    print(robot_description)
+
+    robot_state_publisher = Node(
+             package='robot_state_publisher',
+             executable='robot_state_publisher',
+             name='robot_state_publisher',
+             output='screen',
+             parameters=[{'use_sim_time': True, 'robot_description': robot_description, 'frame_prefix' : 'Eddy2a/'}],
+    )
 
     return LaunchDescription([
         IncludeLaunchDescription(
@@ -23,19 +33,12 @@ def generate_launch_description():
                                            'launch', 'gazebo.launch.py'),
              )
         ),
-
-        Node(
-             package='robot_state_publisher',
-             executable='robot_state_publisher',
-             name='robot_state_publisher',
-             output='screen',
-             parameters=[{'use_sim_time': True, 'robot_description': robot_desc}],
-             arguments=[urdf]),
+        robot_state_publisher,
         Node(
              package='gazebo_ros',
              executable='spawn_entity.py',
              name='urdf_spawner',
              output='screen',
-             arguments=["-topic", "/robot_description",  "-entity",  "eddy2A"]),
+             arguments=["-topic", "/robot_description",  "-entity",  "Eddy2a"]),
     ])
 
---
+++ ros2_ws/src/eddy_gazebo/launch/stong-pond.launch.py
@@ -0,0 +1,46 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import xacro
+import os
+
+def generate_launch_description():
+
+    # get the robot description
+    urdf = os.path.join(get_package_share_directory('eddy_description'), 'eddy.urdf.xacro')
+    robot_description = xacro.process_file(urdf , mappings={ 'robot_name' : 'Eddy2a' }).toxml()
+    robot_state_publisher = Node(
+             package='robot_state_publisher',
+             executable='robot_state_publisher',
+             name='robot_state_publisher',
+             output='screen',
+             parameters=[{'use_sim_time': True, 'robot_description': robot_description, 'frame_prefix' : 'Eddy2a/'}],
+    )
+
+    # get a description of Gazebo with the world required
+    world = os.path.join(get_package_share_directory('eddy_gazebo'), 'stong_pond.world')
+    gazebo_launch = IncludeLaunchDescription(
+             PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('gazebo_ros'),
+                                           'launch', 'gazebo.launch.py'),
+             ),
+             launch_arguments = {'world': world}.items()
+        )
+
+    # spawn the robot
+    spawn_robot = Node(
+             package='gazebo_ros',
+             executable='spawn_entity.py',
+             name='urdf_spawner',
+             output='screen',
+             arguments=["-topic", "/robot_description",  "-entity",  "Eddy2a"])
+
+    return LaunchDescription([
+        gazebo_launch, 
+        robot_state_publisher,
+        spawn_robot,
+    ])
+
