3dd6fe4557279b591ee2cfc8860eca46e9a66ec2
Michael Jenkin <jenkin@yorku.ca>
2024-05-30 20:00:11 +0000

    more tweaking on the web_gui

--- ros2_ws/src/eddy_webui/eddy_webui/web_ui_node.py
+++ ros2_ws/src/eddy_webui/eddy_webui/web_ui_node.py
@@ -2,42 +2,36 @@ import rclpy
 from rclpy.node import Node
 from rclpy.parameter import Parameter
 from geometry_msgs.msg import Twist
-from threading import Thread, Lock
-from time import sleep
+import http.server
+import socketserver
 
+# hack
+class Handler(http.server.SimpleHTTPRequestHandler):
+    ROOT= "."
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=Handler.ROOT, **kwargs)
 
-from geometry_msgs.msg import PointStamped
 
 class WebUI(Node):
     def __init__(self):
         super().__init__('eddy_webui')
         self.get_logger().info(f'{self.get_name()} created')
 
-        self.declare_parameter('www_root', "~/www")
+        self.declare_parameter('root', "/home/jenkin/Documents/Eddy2.0/ros2_ws/src/eddy_webui/www/")
+        root = self.get_parameter('root').get_parameter_value().string_value
 
-        self._www_root = self.get_parameter('www_root').get_parameter_value().string_value
-        self._target_topic = self.get_parameter('www_root').get_parameter_value().string_value
+        self.declare_parameter('port', 8081)
+        port = self.get_parameter('port').get_parameter_value().integer_value
 
-        self._thread = Thread(target = self._server)
-        self._lock = Lock()
-        self._thread.start()
- 
-
-    def _server(self):
-        while True:
-            self.get_logger().info(f'{self.get_name()} sleeping')
-            sleep(1)
-            
-        
-        
-
-
+        Handler.ROOT = root
+        with socketserver.TCPServer(("", port), Handler) as httpd:
+            self.get_logger().info(f'{self.get_name()} serving')
+            httpd.serve_forever()
 
 def main(args=None):
     rclpy.init(args=args)
-    node = WebUI()
     try:
-        rclpy.spin(node)
+        node = WebUI()
         rclpy.shutdown()
     except KeyboardInterrupt:
         pass
--- ros2_ws/src/eddy_webui/setup.py
+++ ros2_ws/src/eddy_webui/setup.py
@@ -4,18 +4,19 @@ package_name = 'eddy_webui'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jenkin',
     maintainer_email='jenkin@yorku.ca',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Provide web-based support to introspect and control the Eddy robots',
+    license='MIT license',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
